7a379875cfa470bd83069f64f17421c408902c44
Nicolas De Jay <ndj@pinkfilter.org>
2015-07-20 00:02:39 -0400

    Build archive index generator.

--- _plugins/generate_archive_index.rb
+++ _plugins/generate_archive_index.rb
@@ -18,11 +18,49 @@ module Jekyll
 
   class ArchiveIndex < Page
 
-    def initialize(site, base, dir, name, data = {})
-      self.content = data.delete('content') || ''
-      self.data    = data
+    def initialize(site, base, dir, name, data)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = name
+      @data = data
 
-      super(site, base, dir[-1, 1] == '/' ? dir : '/' + dir, name)
+      process(@name)
+
+      @data['archive_tags'] = generate_archive_tags
+      @data['archive_date'] = generate_archive_for_years
+    end
+
+    def generate_archive_tags
+      tags = site.posts.map { |p| p.tags }.inject(&:+).uniq
+      tags.map do |t| 
+        {
+          'url'  => "/tag/#{t}",
+          'name' => t,
+        }
+      end
+    end
+
+    def generate_archive_for_years
+      years = site.posts.map { |p| p.url_placeholders[:year] }.uniq.sort
+      years.map do |y|
+        {
+          'url'    => "/#{y}",
+          'name'   => y,
+          'months' => generate_archive_for_months_in_year(y),
+        }
+      end
+    end
+
+    def generate_archive_for_months_in_year(year)
+      posts_in_year = site.posts.select { |p| p.url_placeholders[:year] == year }
+      months = posts_in_year.map { |p| p.url_placeholders[:month] }.uniq.sort
+      months = months.map do |m|
+        {
+          'url'  => "/#{year}/#{m}",
+          'name' => Time.new(0, m).strftime("%B"),
+        }
+      end
     end
 
     def read_yaml(*) ; end
